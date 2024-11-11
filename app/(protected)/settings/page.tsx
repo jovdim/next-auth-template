66ecca8a583128dbd11845cdaebc7f532aea79c9
ipsum <markjovendimaculangan77@gmail.com>
2024-11-11 17:46:41 +0800

    feat: completed  settings page with real userdata

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,38 @@
-import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { UserAccount } from "@/components/UserAccount";
+
+type Session = {
+  user: {
+    name: string;
+    email: string;
+    image: string;
+  } | null;
+};
+
+type userData = {
+  name: string;
+  email: string;
+  image: string;
+};
 
 export default async function page() {
   const session = await auth();
+
+  // If no session or user is found, return early
+  if (!session || !session.user) return;
+
+  const { name, email, image } = session.user;
+
+  // If any of the user fields are missing, return early
+  if (!name || !email || !image) return;
+
+  // Create a typed data object
+  const data: userData = { name, email, image };
+
   return (
     <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/" });
-        }}
-      >
-        <Button type="submit" variant="logInButton" className="w-28">
-          Log out
-        </Button>
-      </form>
+      <UserAccount data={data} />
     </div>
   );
 }
