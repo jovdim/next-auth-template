3d0541fb39d876c56581c92ed5b0a45f45183b0f
mark joven dimaculangan <145369597+jovdim@users.noreply.github.com>
2024-11-24 20:23:45 +0800

    Update UserAccount.tsx

--- components/UserAccount.tsx
+++ components/UserAccount.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Card,
   CardContent,
